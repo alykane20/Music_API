b3d6582f6415c846ce6700b0f249a3ee8b6273e8
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-04-25 12:50:37 -0400

    Created super user + connected admin site to music admin.py file

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
 
 @api_view(['GET'])
 def song_list(request):
+    
+    songs = Song.objects.all()
 
-    return Response('HI')
+    serializer = SongSerializer(songs, many=True)
+
+    return Response(serializer.data)
