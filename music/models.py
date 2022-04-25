1141f31a228d6076f82de0e6d965e18fd7e8be22
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-04-25 12:02:39 -0400

    Created Song Model + registed Song and rest_framework in App section

---
+++ music/models.py
@@ -0,0 +1,10 @@
+from email import charset
+from django.db import models
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=300)
+
